61b8f6122967244b73f2c4792f394bd6f6addf08
iam <smb@mail.ru>
2024-02-22 09:24:14 +0200

    xx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Index(props) {
   const filteredArticles = filterArticles(articles, selectedTag);
 
 
-  const [views, setViews] = useState([])
+  const [views, setViews] = useState()
   //
   async function getViews() {
     const views = (
@@ -57,6 +57,7 @@ export default function Index(props) {
   //
   useEffect( () => {
     getViews().then((request) => {
+      // @ts-ignore
       setViews(request)
     })
   },[])
